d54e2aa43d31b475f4e2511cc401979737277146
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-11-12 21:09:02 +0300

    Initial Commit

---
+++ BookBridge/BookBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBridgeApp.swift
+//  BookBridge
+//
+//  Created by Beste Kocaoglu on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
